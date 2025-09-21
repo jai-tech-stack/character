fc9426937dfa52588bb8721762a58e044ac8d97a
jai-tech-stack <jaik233t@gmail.com>
2025-09-21 10:46:00 +0530

    url e

--- src/hooks/useTTS.js
+++ src/hooks/useTTS.js
@@ -1,5 +1,5 @@
 // src/api/chatApi.js
-const API_URL = "https://character-chan.onrender.com" 
+const API_URL = "https://character-chan.onrender.com";
 //import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 console.log('🔍 API_URL being used:', API_URL);
